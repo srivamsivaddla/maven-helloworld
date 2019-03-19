a95129fb72d14773e3eee4f2d65a8a8b8407f41b
Cloud User <ec2-user@ip-172-31-29-107.ap-south-1.compute.internal>
2019-03-19 07:35:24 +0000

    Reverted Back to Original Version

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-	System.out.println("AutoDeployment With Jenkins");
+	/*System.out.println("AutoDeployment With Jenkins");*/
     }
 
     private final String getMessage() {
